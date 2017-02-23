17bd3acb6e50737462b086b4354b3aa809cbcf4c
cristhianferreira <cristhiancougo@hotmail.com>
2017-02-23 10:42:26 -0300

    aula 1 23022017

--- Ponto.java
+++ Ponto.java
@@ -1,5 +1,13 @@
 public class Ponto{
 	private double x,y;
+	public Ponto(){
+		System.out.println("Contrutor 1 -"+ this);
+	}
+	public Ponto(double x){
+		System.out.println("Contrutor 2");
+	this.x=x;	
+	this.y=y;
+	}
 	public void setX(double x){
     	this.x = x;
 	}
@@ -16,6 +24,7 @@ public class Ponto{
 		Ponto  p1 = new Ponto();
 		p1.setX(10);
 		p1.setY(20); 
+		Ponto  p3 = new Ponto(4);
 		Ponto  p2 = new Ponto();		
         Ponto  p4 = new Ponto();
 		p4.setX(70);
@@ -29,5 +38,15 @@ public class Ponto{
         double dis1= al*al+ lar*lar;
         double dis2 = Math.sqrt(dis1);
         System.out.println("Distancia entre as p4 e p1= "+ dis2);
+     	Reta r1 = new Reta();
+		r1.setP1(p1);
+		r1.setP2(p2);
+ 		p1.setX(30);
+		r1.getP2().setY(40);
+		System.out.println("Pontos reta r1");
+		System.out.println("Pontos p1 x= "+r1.getP1().getX());
+     	System.out.println("Pontos p1 y= "+r1.getP1().getY());
+		System.out.println("Pontos p2 x= "+r1.getP2().getX());
+		System.out.println("Pontos p2 y= "+r1.getP2().getY());
 	}
 }
