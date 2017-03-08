2696db83f42aaecdb6bc4cf6945dc6ca967c6edd
cristhianferreira <cristhiancougo@hotmail.com>
2017-03-08 11:18:52 -0300

    aula array

---
+++ ep3/Principal.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;	
+import java.util.Arrays;
+public class Principal{
+	public static void main(){
+	ArrayList< Aluno> alunos = new ArrayList< Aluno >();
+
+	Aluno alunoa = new Aluno();	
+	alunoa.setNome("Joaozinho");	
+	alunoa.setIdade(15);	
+	alunos.add (alunoa);
+
+	Aluno alunob = new Aluno();	
+	alunob.setNome("Joaozinho");	
+	alunob.setIdade(15);	
+	alunos.add (alunob);
+
+	Aluno alunoc = new Aluno();	
+	alunoc.setNome("Joaozinho");	
+	alunoc.setIdade(15);	
+	alunos.add (alunoc);
+
+	Aluno alunod = new Aluno();	
+	alunod.setNome("Joaozinho");	
+	alunod.setIdade(15);	
+	alunos.add (alunod);
+
+	Aluno alunoe = new Aluno();	
+	alunoe.setNome("Joaozinho");	
+	alunoe.setIdade(15);	
+	alunos.add (alunoe);
+
+	}
+
+
+}
