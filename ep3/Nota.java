349c50b49a14309d942e4f0be1d31f0fd222a665
cristhianferreira <cristhiancougo@hotmail.com>
2017-03-19 12:05:57 -0300

    Update Nota.java

--- ep3/Nota.java
+++ ep3/Nota.java
@@ -1,8 +1,21 @@
-public class Nota {
+import java.util.Arrays;
+public class Nota implements Comparable<Notas>{
 	private double valor;		
 	private Aluno aluno;	
 	private Disciplina disciplina;	
 	
+	public Notas( double valor ,Aluno aluno,Disciplina disciplina){
+		this.valor = valor;
+		this.aluno = aluno;
+		this.disciplina = disciplina;
+	}
+	public void setAluno(Aluno aluno){
+		this.aluno=aluno;
+	}
+
+	public Aluno getAluno(){	
+		return this.aluno;
+	}	
 	public void setValor(double valor){
 		this.valor=valor;
 	}
@@ -11,21 +24,22 @@ public class Nota {
 		return this.valor;
 	}	
 
-	public void setAluno(Aluno aluno){
-		this.aluno=aluno;
-	}
-
-	public Aluno getAluno(){	
-		return this.aluno;
-	}	
-	
 	public void setDisciplina(Disciplina disciplina){
 		this.disciplina=disciplina;
 	}
 
 	public Disciplina getDisciplina(){	
 		return this.disciplina;
-	}	
+	}
+	public int compareTo(Notas outro){
+	if(this.valor < outro.getValor()){
+		return 1;
+	}else if (this.valor > outro.getValor()){
+		return -1;
+	}else{
+		return 0;
+	}
+}
 	
 }
 
