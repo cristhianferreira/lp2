23af42501b96570e14e162980a7d721f6b5f22fa
cristhianferreira <cristhiancougo@hotmail.com>
2017-04-17 11:34:08 -0300

    Add files via upload

--- Exemplos/ProgramaException2.java
+++ Exemplos/ProgramaException2.java
@@ -1,21 +1,21 @@
 public class ProgramaException2{
-	private int variavel;
+	public static void teste() throws NullPointerException{
+		int[] vetor1=new int[5];
+		try{
+			vetor1[6]=10;		
+		}
+		catch(Exception e){
+			throw new NullPointerException();
+		}
+	}
+
 
 	public static void main(String [] args){
-		testeException2();
-	} 
-	public static void testeException2(){
-		int[] vetor1= new int[5];
-		System.out.println("Teste exception 2");
 		try{
-			vetor1[6] = 10;	
-			System.out.println("Teste exception - Apos o erro");
+			teste();
 		}
-		catch(ArrayIndexOutOfBoundsException e){
-			System.out.println("Um erro ocorre erro foi:");				
+		catch(NullPointerException e){		
+				System.out.println("Ocorreu um erro no sistema");
 		}
-	}	
-
-
-
+	}
 }
